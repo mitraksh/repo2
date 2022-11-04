521c3560f80ea117c2f930b90e1ae0e0ab582849
mitraksh <rathodmitraksh@gmail.com>
2022-11-04 12:51:57 +0530

    indexrepo2 changes (temporary2)

--- indexrepo2.js
+++ indexrepo2.js
@@ -3,34 +3,18 @@ const fs = require("fs-extra");
 const path = require("path");
 const { exec } = require("child_process");
 const packagerepo2 = require('./package.json');
-const packagerepo1 = require('../repo1/package.json');
 
 let repo2Path = path.join("/home/mitraksh/dev/mitraksh/scopedtask/repo2");
 //define repo1 path and repo1 object path
 let repo1Path = path.join("/home/mitraksh/dev/mitraksh/scopedtask/repo1");
 let repo1ObjPath = path.join(repo1Path, "myobject.json");
-/*
-// * pre prcoess arguments function
-function processArgs() {
-  program.option(`-r2, --repo2 ${repo2_version}`);
-  program.parse();
-
-  const options = program.opts();
-
-  return repo2_version;
-}
-
-console.log(processArgs()+' process');
-*/
 
 // * main function
 
 async function main() {
-  //const options = processArgs();
-  //console.log(options+' options')
   try {
     exec(
-      `cd ${repo2Path} && npm version patch && npm publish`,
+      `cd ${repo2Path} && npm version patch`,
       //&& git commit -am "update version" && git push origin master`,
       //&& cd ${repo1Path} git fetch origin master && git commit -am 'pre relese' && npm version patch && git push origin master`,
       (err, stdout, stderr) => {
@@ -51,7 +35,7 @@ async function main() {
     result.repo2 = repo2_version;
     await fs.writeJSON(repo1ObjPath, result);
     exec(
-      `cd ${repo1Path}  && git commit -am 'pre release' && npm version patch`,
+      `cd ${repo1Path}  && git commit -am 'pre relese' && npm version patch && git push origin master`,
       (err, stdout, stderr) => {
         if (err) {
           console.error(err);
@@ -59,24 +43,6 @@ async function main() {
           console.log(`stdout: ${stdout}`);
           console.log(`stderr: ${stderr}`);
          console.log('v+'+repo2_version);
-        }
-      }
-    );
-    const repo1_version = packagerepo2.version;
-    let result1 = await fs.readFile(repo1ObjPath, "utf8");
-    console.log(result)
-    result = JSON.parse(result);
-    result.repo1 = repo1_version;
-    await fs.writeJSON(repo1ObjPath, result);
-    exec(
-      `cd ${repo1Path}  && git commit -am 'pre release' && git push origin master`,
-      (err, stdout, stderr) => {
-        if (err) {
-          console.error(err);
-        } else if(!err) {
-          console.log(`stdout: ${stdout}`);
-          console.log(`stderr: ${stderr}`);
-         console.log('v+'+repo1_version);
         }
       }
     );
