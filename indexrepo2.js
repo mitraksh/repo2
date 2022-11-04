947474f662e91256c1053b8388e43c605163fcf3
mitraksh <rathodmitraksh@gmail.com>
2022-11-04 11:19:05 +0530

    update version

---
+++ indexrepo2.js
@@ -0,0 +1,55 @@
+const { program } = require("commander");
+const fs = require("fs-extra");
+const path = require("path");
+const { exec } = require("child_process");
+const packagerepo2 = require('./package.json');
+const repo2_version = packagerepo2.version;
+console.log(repo2_version);
+
+let repo2Path = path.join("/home/mitraksh/dev/mitraksh/scopedtask/repo2");
+let repo2ObjPath = path.join(repo2Path, "myobject.json");
+// ? repo1 path and repo1 object apth
+let repo1Path = path.join("/home/mitraksh/dev/mitraksh/scopedtask/repo1");
+let repo1ObjPath = path.join(repo1Path, "myobject.json");
+
+// * pre prcoess arguments function
+function processArgs() {
+  program.option(`-r2, --repo2 ${repo2_version}`);
+  program.parse();
+
+  const options = program.opts();
+
+  return repo2_version;
+}
+
+console.log(processArgs()+' process');
+
+// * main function
+
+async function main() {
+  const options = processArgs();
+  console.log(options+' options')
+  try {
+    let result = await fs.readFile(repo1ObjPath, "utf8");
+    console.log(result)
+    result = JSON.parse(result);
+    result.repo2 = options;
+    //result.key2 = options.key2 || result.key2;
+    await fs.writeJSON(repo1ObjPath, result);
+    /*exec(
+      //`cd ${repo1Path} git fetch origin master && git commit -am 'pre relese' && npm version patch && git push origin master`,
+      (err, stdout, stderr) => {
+        if (err) {
+          console.error(err);
+        } else {
+          console.log(`stdout: ${stdout}`);
+          console.log(`stderr: ${stderr}`);
+        }
+      }
+    );*/
+  } catch (error) {
+    throw new Error(error);
+  }
+}
+
+main();
